f41d0c96ad3332433a0158f1e6626588ebebfd57
thanhbat96 <thanhbat96@gmail.com>
2019-05-15 11:33:23 +0700

    update

--- src/main/java/com/ltt/staffmanager/repository/StaffRepository.java
+++ src/main/java/com/ltt/staffmanager/repository/StaffRepository.java
@@ -11,14 +11,14 @@ import java.util.List;
 public interface StaffRepository extends CrudRepository<StaffEntity, Long> {
     List<StaffEntity> findByName(String name);
 
-    @Query("from StaffEntity st where st.name like %:name% " +
-            "and (:startDate is null or st.birthday >=:startDate) " +
-            "and (:endDate is null or st.birthday <= :endDate) " +
-            "and st.phonenumber like %:phonenumber% and st.address like %:address%")
-//    @Query(value = "select * from staff where name like :name " +
-//            "and ( birthday >= :startDate)" +
-//            "and ( birthday <= :endDate)" +
-//            "and phonenumber like :phonenumber and address like :address", nativeQuery = true)
+//    @Query("from StaffEntity st where st.name like %:name% " +
+//            "and (:startDate is null or st.birthday >=:startDate) " +
+//            "and (:endDate is null or st.birthday <= :endDate) " +
+//            "and st.phonenumber like %:phonenumber% and st.address like %:address%")
+    @Query(value = "select * from staff where name like :name " +
+            "and ( birthday >= :startDate)" +
+            "and ( birthday <= :endDate)" +
+            "and phonenumber like :phonenumber and address like :address", nativeQuery = true)
     List<StaffEntity> searchByProperties(@Param("name") String name,
                                          @Param("startDate") Date startDate,
                                          @Param("endDate") Date endDate,
